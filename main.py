57c18ea19222b5eff21d8fb4fd70d8a4eb0b2887
Merve27-coder <mervekoyunyerli@gmail.com>
2024-02-14 21:26:42 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,134 @@
+print("Merhaba Tobeto Test Ekibi")
+
+text  = "Hello "
+kullaniciAdi = "Merve"
+print(text + kullaniciAdi)
+
+studentCount = "45"
+print(studentCount + "5")
+
+studentCount1 = 5
+print(studentCount1 + 5)
+
+averagePoint = 25.5
+print(averagePoint + 5)
+
+isVerified = True
+print(isVerified)
+
+print(type(text))
+
+number = 10
+print(10+10)
+print(number+number)
+
+print(number-5)
+
+print( number/ 2)
+
+print(number*3)
+
+print(number % 3)
+
+print(number==10)
+print(number)
+
+print(number ==10)
+print(number !=10)
+
+print(number>10)
+print(number >=10)
+
+text  = "Hello "
+kullaniciAdi = "Merve"
+print(text + kullaniciAdi)
+totalText = text + " "+ kullaniciAdi
+print(totalText)
+
+
+totalText= f"Hoşgeldiniz {kullaniciAdi}"
+print(totalText)
+
+sayilar = [100,200,300,"Merhaba",True,15.5]
+print(sayilar[1])
+
+print(sayilar)
+
+sayilar.append(400)
+print(sayilar)
+
+
+sayilar.remove("Merhaba")
+print(sayilar)
+
+sayilar.pop(2)
+print(sayilar)
+
+add = [700,800,900]
+sayilar.extend(add)
+print(sayilar)
+
+
+sayilar.clear()
+print(sayilar)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
